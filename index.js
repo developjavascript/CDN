9693d2fbe07245415d52f48bff6f8269e759ca84
Jamen Marz <jamen.marz@gmail.com>
2015-05-23 01:29:05 -0600

    Implemented tools into wrapper

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 "use strict";
+var lib = require("./lib"), tools
 var CDN = function(req, res){
-  this.lib = require("./lib") || {};
+  tools = new lib.tools(req, res);
+  req.tools = tools.req,
+  res.tools = tools.res;
 
-  
+  res.end(req.tools.mime());
 };
 
 module.exports = exports = CDN;
